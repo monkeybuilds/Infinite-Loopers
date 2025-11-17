de6c19337ed8a31fe7ad735970085a1e0769a46d
Atharv <atharvpathak87@gmail.com>
2025-11-17 16:51:27 +0530

    Frontend

---
+++ README.md
@@ -0,0 +1,11 @@
+
+  # CarbonSetu Web Platform UI/UX
+
+  This is a code bundle for CarbonSetu Web Platform UI/UX. The original project is available at https://www.figma.com/design/iCLY0oqVbaPv0SxUeKBNY8/CarbonSetu-Web-Platform-UI-UX.
+
+  ## Running the code
+
+  Run `npm i` to install the dependencies.
+
+  Run `npm run dev` to start the development server.
+  
---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Navbar } from './components/Navbar';
+import { Home } from './components/pages/Home';
+import { Registration } from './components/pages/Registration';
+import { Dashboard } from './components/pages/Dashboard';
+import { DigitalTwin } from './components/pages/DigitalTwin';
+import { Marketplace } from './components/pages/Marketplace';
+import { Certificate } from './components/pages/Certificate';
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home onNavigate={setCurrentPage} />;
+      case 'registration':
+        return <Registration onNavigate={setCurrentPage} />;
+      case 'dashboard':
+        return <Dashboard />;
+      case 'digitaltwin':
+        return <DigitalTwin />;
+      case 'marketplace':
+        return <Marketplace />;
+      case 'certificate':
+        return <Certificate />;
+      default:
+        return <Home onNavigate={setCurrentPage} />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen bg-white">
+      <Navbar currentPage={currentPage} onNavigate={setCurrentPage} />
+      <main>{renderPage()}</main>
+    </div>
+  );
+}
